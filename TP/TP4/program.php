9d796d1e8803e157154a47e4b0ec9b96c79c7d5c
JStudent97 <teodordeaconu2014@gmail.com>
2019-03-12 13:28:16 +0200

    php and txt files

---
+++ TP/TP4/program.php
@@ -0,0 +1,18 @@
+if ($v == 4)
+{
+    $v = $v1 + 1;
+    $v = $v2 - 1;
+    $v = $v1 * 3;
+    $v = $v1 / 2;
+}
+
+function s ()
+{
+    print ($s);
+}
+
+while ($v)
+{
+    print ($v);
+    $v = $v (+1);
+}
